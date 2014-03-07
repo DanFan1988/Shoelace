c5a30b5b7530376a9ca8bcc3f2662072958dd9b4
Dan Fan <dan.fan1988@gmail.com>
2014-03-06 18:40:39 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoelace::Application.config.secret_token = '7c376560dd303b6af0ebfa170ac67781dcf653b4480e7b1468867effb06120e371a94f7c540c6099bc2c3a5374cc0871b63c1ea41b8bec426a11154f43444c12'
